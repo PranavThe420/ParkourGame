e4aa8d9329271d23b9dd33647767c377aab7a5d3
JazzyMrDerp <88635779+JazzyMrDerp@users.noreply.github.com>
2022-07-12 14:37:51 -0400

    movement_finished

--- My project/Assets/Movement.cs
+++ My project/Assets/Movement.cs
@@ -1,21 +1,46 @@
-using System.Collections;
+ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     public CharacterController controller;
+    
     public float speed = 12f;
-  
+    public float gravity = -9.81f;
+    public float jumpheight = 3f;
+
+
+    public Transform groundcheck;
+    public float grounddistance = 0.4f;
+    public LayerMask groundMask;
+    bool isgrounded;
+
+
+    Vector3 velocity;
     // Update is called once per frame
     void Update()
     {
+
+        isgrounded = Physics.CheckSphere(groundcheck.position, grounddistance, groundMask);
+
+        if(isgrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
         float x = Input.GetAxis("Horizontal");
-        float z = Input.GetAxis("Verical");
+        float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
 
         controller.Move(move * speed * Time.deltaTime);
 
+        if(Input.GetButtonDown("Jump") && isgrounded)
+        {
+            velocity.y = Mathf.Sqrt(3f * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+        controller.Move(velocity * Time.deltaTime);
     }
 }
